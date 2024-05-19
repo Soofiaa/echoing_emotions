c69c9dba6737298f23fbdbba7af511980e45d828
JatsuryNeira <jatsurygenesisneira@gmail.com>
2024-05-19 19:46:47 -0400

    cambios EntradaDibujo

--- lib/pantalla_entrada_dibujo.dart
+++ lib/pantalla_entrada_dibujo.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
-import 'dart:ui';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'dart:ui'; // Importación de dart:ui para PointMode
+
+class DrawingPoint {
+  Offset offset;
+  Paint paint;
+
+  DrawingPoint(this.offset, this.paint);
+}
 
 class EntradaDibujo extends StatefulWidget {
-  final List<Offset?> initialPoints;
+  final List<DrawingPoint?> initialPoints;
 
   EntradaDibujo({required this.initialPoints});
 
@@ -11,67 +19,134 @@ class EntradaDibujo extends StatefulWidget {
 }
 
 class _EntradaDibujoState extends State<EntradaDibujo> {
-  List<Offset?> points = [];
+  Color selectedColor = Colors.black;
+  double strokeWidth = 5;
+  List<DrawingPoint?> drawingPoints = [];
 
   @override
   void initState() {
     super.initState();
-    points = List.from(widget.initialPoints); // Copiar los puntos iniciales
+    drawingPoints = widget.initialPoints;
+  }
+
+  void saveDrawing(BuildContext context) {
+    Navigator.pop(context, drawingPoints);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Pizarra de Dibujo')),
-      body: GestureDetector(
-        onPanUpdate: (details) {
-          setState(() {
-            points.add(details.localPosition); // Agregar el punto al dibujo
-          });
-        },
-        onPanEnd: (details) {
-          points.add(null); // Marcar el final del trazo
-        },
-        child: CustomPaint(
-          painter: PizarraPainter(points),
-          size: Size.infinite,
+      appBar: AppBar(
+        title: Text('Dibujo'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context, drawingPoints);
+          },
         ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () => saveDrawing(context),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pop(context, points); // Devolver los puntos dibujados
-          print('Dibujo guardado');
-        },
-        child: Icon(Icons.save),
+      body: Stack(
+        children: [
+          GestureDetector(
+            onPanStart: (details) {
+              setState(() {
+                drawingPoints.add(
+                  DrawingPoint(
+                    details.localPosition,
+                    Paint()
+                      ..color = selectedColor
+                      ..isAntiAlias = true
+                      ..strokeWidth = strokeWidth
+                      ..strokeCap = StrokeCap.round,
+                  ),
+                );
+              });
+            },
+            onPanUpdate: (details) {
+              setState(() {
+                drawingPoints.add(
+                  DrawingPoint(
+                    details.localPosition,
+                    Paint()
+                      ..color = selectedColor
+                      ..isAntiAlias = true
+                      ..strokeWidth = strokeWidth
+                      ..strokeCap = StrokeCap.round,
+                  ),
+                );
+              });
+            },
+            onPanEnd: (details) {
+              setState(() {
+                drawingPoints.add(null);
+              });
+            },
+            child: CustomPaint(
+              painter: _DrawingPainter(drawingPoints),
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+              ),
+            ),
+          ),
+          Positioned(
+            top: 40,
+            right: 30,
+            child: Row(
+              children: [
+                Slider(
+                  min: 0,
+                  max: 40,
+                  value: strokeWidth,
+                  onChanged: (val) => setState(() => strokeWidth = val),
+                ),
+                ElevatedButton.icon(
+                  onPressed: () => setState(() => drawingPoints = []),
+                  icon: Icon(Icons.clear),
+                  label: Text("Clear Board"),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          color: Colors.grey[200],
+          padding: EdgeInsets.all(10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [],
+          ),
+        ),
       ),
     );
   }
 }
 
-class PizarraPainter extends CustomPainter {
-  final List<Offset?> points;
+class _DrawingPainter extends CustomPainter {
+  final List<DrawingPoint?> drawingPoints;
 
-  PizarraPainter(this.points);
+  _DrawingPainter(this.drawingPoints);
 
   @override
   void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = Colors.black
-      ..strokeCap = StrokeCap.round
-      ..strokeWidth = 5.0;
-
-    for (int i = 0; i < points.length - 1; i++) {
-      if (points[i] != null && points[i + 1] != null) {
-        canvas.drawLine(points[i]!, points[i + 1]!, paint);
-      } else if (points[i] != null && points[i + 1] == null) {
-        // Dibujar puntos individuales
-        canvas.drawPoints(PointMode.points, [points[i]!], paint);
+    for (int i = 0; i < drawingPoints.length - 1; i++) {
+      if (drawingPoints[i] != null && drawingPoints[i + 1] != null) {
+        canvas.drawLine(drawingPoints[i]!.offset, drawingPoints[i + 1]!.offset,
+            drawingPoints[i]!.paint);
+      } else if (drawingPoints[i] != null && drawingPoints[i + 1] == null) {
+        canvas.drawPoints(PointMode.points, [drawingPoints[i]!.offset], drawingPoints[i]!.paint);
       }
     }
   }
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true;
-  }
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
--- lib/pantalla_nueva_entrada_diario.dart
+++ lib/pantalla_nueva_entrada_diario.dart
@@ -1,8 +1,8 @@
+import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'pantalla_entrada_dibujo.dart'; // Importa la clase de dibujo
-import 'dart:ui';
-
+import 'package:flutter/services.dart';
+import 'pantalla_entrada_dibujo.dart';
 
 class EntradaDiario extends StatefulWidget {
   @override
@@ -11,8 +11,9 @@ class EntradaDiario extends StatefulWidget {
 
 class _EntradaDiarioState extends State<EntradaDiario> {
   TextEditingController _textController = TextEditingController();
-  final double _initialHeight = 100.0; // Altura inicial del contenedor
-  List<Offset?> _drawingPoints = []; // Lista para almacenar los puntos del dibujo
+  TextEditingController _titleController = TextEditingController();
+  final double _initialHeight = 100.0;
+  List<DrawingPoint?> _drawingPoints = [];
 
   @override
   Widget build(BuildContext context) {
@@ -32,12 +33,47 @@ class _EntradaDiarioState extends State<EntradaDiario> {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
-              Flexible(
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: TextField(
+                  controller: _titleController,
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'AbrilFatface',
+                  ),
+                  inputFormatters: [
+                    LengthLimitingTextInputFormatter(50),
+                  ],
+                  decoration: InputDecoration(
+                    hintText: 'Escribe el título aquí',
+                    hintStyle: TextStyle(
+                      fontFamily: 'AbrilFatface',
+                    ),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
                 child: AnimatedContainer(
-                  duration: Duration(milliseconds: 100),
+                  duration: Duration(milliseconds: 50),
                   width: double.infinity,
                   constraints: BoxConstraints(
-                    minHeight: _initialHeight, // Altura mínima
+                    minHeight: _initialHeight,
                   ),
                   decoration: BoxDecoration(
                     color: Colors.white,
@@ -55,53 +91,20 @@ class _EntradaDiarioState extends State<EntradaDiario> {
                     child: TextField(
                       controller: _textController,
                       textAlign: TextAlign.justify,
-                      maxLines: null, // Permite un número ilimitado de líneas
-                      keyboardType: TextInputType.multiline, // Habilita el salto de línea
+                      maxLines: null,
+                      keyboardType: TextInputType.multiline,
                       decoration: InputDecoration(
                         hintText: 'Escribe aquí',
                         border: InputBorder.none,
-                        contentPadding: EdgeInsets.all(8.0),
+                        contentPadding: EdgeInsets.all(20.0),
                       ),
                     ),
                   ),
                 ),
               ),
               SizedBox(height: 20),
-              // Mostrar el dibujo guardado como una pizarra pequeña
-              GestureDetector(
-                onTap: () async {
-                  final result = await Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => EntradaDibujo(initialPoints: _drawingPoints)),
-                  );
-                  if (result != null) {
-                    setState(() {
-                      _drawingPoints = result;
-                    });
-                  }
-                },
-                child: Container(
-                  height: 150, // Altura de la pizarra pequeña
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 5,
-                        blurRadius: 5,
-                        offset: Offset(0, 3),
-                      ),
-                    ],
-                  ),
-                  child: _drawingPoints.isNotEmpty
-                      ? CustomPaint(
-                    painter: PizarraPainter(_drawingPoints),
-                  )
-                      : Center(child: Text('Toca para dibujar')),
-                ),
-              ),
+              _buildDrawingStack(),
+              SizedBox(height: 20),
             ],
           ),
         ),
@@ -119,51 +122,54 @@ class _EntradaDiarioState extends State<EntradaDiario> {
             });
           }
         },
-        child: Icon(Icons.edit), // Icono de lápiz
+        child: Icon(Icons.edit),
         backgroundColor: Colors.teal,
-      ),
-      bottomNavigationBar: BottomAppBar(
-        shape: CircularNotchedRectangle(),
-        notchMargin: 8.0,
-        color: Colors.white,
-        child: Container(
-          height: 60.0,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Puedes agregar otros widgets aquí si es necesario
-            ],
-          ),
-        ),
       ),
     );
   }
+
+  Widget _buildDrawingStack() {
+    return Stack(
+      alignment: Alignment.centerLeft,
+      children: [
+        if (_drawingPoints.isNotEmpty)
+          Container(
+            color: Colors.transparent,
+
+          ),
+        if (_drawingPoints.isNotEmpty)
+          Positioned(
+            left: 0,
+            child: Transform.scale(
+              scale: 0.5, // Factor de escala
+              child: CustomPaint(
+                painter: _DrawingPainter(_drawingPoints),
+
+              ),
+            ),
+          ),
+      ],
+    );
+  }
+
 }
+class _DrawingPainter extends CustomPainter {
+  final List<DrawingPoint?> drawingPoints;
 
-class PizarraPainter extends CustomPainter {
-  final List<Offset?> points;
-
-  PizarraPainter(this.points);
+  _DrawingPainter(this.drawingPoints);
 
   @override
   void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = Colors.black
-      ..strokeCap = StrokeCap.round
-      ..strokeWidth = 5.0;
-
-    for (int i = 0; i < points.length - 1; i++) {
-      if (points[i] != null && points[i + 1] != null) {
-        canvas.drawLine(points[i]!, points[i + 1]!, paint);
-      } else if (points[i] != null && points[i + 1] == null) {
-        // Dibujar puntos individuales
-        canvas.drawPoints(PointMode.points, [points[i]!], paint);
+    for (int i = 0; i < drawingPoints.length - 1; i++) {
+      if (drawingPoints[i] != null && drawingPoints[i + 1] != null) {
+        canvas.drawLine(drawingPoints[i]!.offset, drawingPoints[i + 1]!.offset,
+            drawingPoints[i]!.paint);
+      } else if (drawingPoints[i] != null && drawingPoints[i + 1] == null) {
+        canvas.drawPoints(PointMode.points, [drawingPoints[i]!.offset], drawingPoints[i]!.paint);
       }
     }
   }
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true;
-  }
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
