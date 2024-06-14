06f6087a72129c0feb21954cbd4321d1b4a6afd7
Estdimer <115674299+Estdimer@users.noreply.github.com>
2024-06-13 22:55:49 -0400

    se arreglo el bug de que se guardaba los cambios siempre en la ultima fecha y borre el botn usuario

--- lib/pantalla_nueva_entrada_diario.dart
+++ lib/pantalla_nueva_entrada_diario.dart
@@ -368,6 +368,7 @@ class _EntradaDiarioState extends State<EntradaDiario> {
     final audioPath = _audioPath;
     final emocion = _emocion;
     final emoji = _emoji;
+    var fechaAnterio;
 
     final nuevaEntrada = Entrada(
       id_entrada: widget.evento != null ? widget.evento!.id : null,
@@ -376,7 +377,7 @@ class _EntradaDiarioState extends State<EntradaDiario> {
       contenido: content,
       dibujo: drawingPoints,
       audio: audioPath,
-      fecha: formattedDate,
+      fecha: widget.evento != null ? widget.evento!.fecha : formattedDate,
       emocion: emocion,
       emoji: emoji,
     );
